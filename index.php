aa3d8a1515331cf4f7a457df0a99b936bae27304
Duvan_Molina <dmmolina45@misena.edu.co>
2021-04-27 15:05:31 -0500

    Update index.php

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
 	  require_once ("Libraries/Core/Autoload.php");
 	  require_once ("Libraries/Core/Load.php");
 
-	echo $url." (((soy el index)))) HOLATEDDY";
+	echo $url." (((soy el index)))) hii";
 
 
 
